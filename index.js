a9545ff8e166ed19bc3602c9ad1cf1e0ba844235
EclipseGame18 <famousferguslive@gmail.com>
2023-04-20 15:36:54 +1000

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, GatewayIntentBits} = require('discord.js');
+const {Client, GatewayIntentBits, ActivityType} = require('discord.js');
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
